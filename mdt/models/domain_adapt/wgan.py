fix: adapter linear zero_init gets zero gradients

mdt/models/domain_adapt/wgan.py
@@ -138,9 +138,10 @@ class AdaLNZero(nn.Module):
         # Initialize weights and biases to zero
         weight_shape = list(self.modulation[1].weight.data.shape)
         weight_shape[0] = weight_shape[0] // self.parts
-        self.modulation[1].weight = torch.nn.Parameter(torch.cat(
-            [torch.zeros(weight_shape) for _ in range(self.parts)]
-        ))  # shift=0,scale=0,gate=0
+        # self.modulation[1].weight = torch.nn.Parameter(torch.cat(
+        #     [torch.zeros(weight_shape) for _ in range(self.parts)]
+        # ))  # shift=0,scale=0,gate=0
+        nn.init.normal_(self.modulation[1].weight, std=0.02)
         nn.init.zeros_(self.modulation[1].bias)
     def forward(self, c):
         return self.modulation(c).chunk(self.parts, dim=-1)  # shift, scale, gate

mdt/models/networks/transformers/transformer_blocks.py
@@ -348,7 +348,7 @@ class ConditionedBlock(Block):
                 self.ff_down = nn.Linear(in_dim, inner_dim, bias=False)
                 self.activation = nn.GELU()
                 self.ff_up = nn.Linear(inner_dim, in_dim, bias=False)
-                torch.nn.init.zeros_(self.ff_down.weight)
+                torch.nn.init.normal_(self.ff_down.weight, std=0.02)
                 torch.nn.init.zeros_(self.ff_up.weight)
             def forward(self, x):
                 return x + self.ff_up(self.activation(self.ff_down(x)))